579d71d72dbe85507ea4e4b33570e2b07947f8f6
pseudowasabi <pseudowasabi@kakao.com>
2025-10-07 19:52:29 +0900

    add module 'api-gateway-webflux'

---
+++ api-gateway-webflux/src/main/java/com/pseudowasabi/apigatewaywebflux/ApiGatewayWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.pseudowasabi.apigatewaywebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayWebfluxApplication.class, args);
+    }
+
+}
---
+++ api-gateway-webflux/src/test/java/com/pseudowasabi/apigatewaywebflux/ApiGatewayWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pseudowasabi.apigatewaywebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
