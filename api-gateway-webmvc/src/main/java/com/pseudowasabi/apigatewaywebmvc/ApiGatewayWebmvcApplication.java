70d915018414bfb988d661d8cf240d8bce58bc97
pseudowasabi <pseudowasabi@kakao.com>
2025-10-07 19:16:29 +0900

    add module 'api-gateway-webmvc'

---
+++ api-gateway-webmvc/src/main/java/com/pseudowasabi/apigatewaywebmvc/ApiGatewayWebmvcApplication.java
@@ -0,0 +1,13 @@
+package com.pseudowasabi.apigatewaywebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayWebmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayWebmvcApplication.class, args);
+    }
+
+}
---
+++ api-gateway-webmvc/src/test/java/com/pseudowasabi/apigatewaywebmvc/ApiGatewayWebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pseudowasabi.apigatewaywebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayWebmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
