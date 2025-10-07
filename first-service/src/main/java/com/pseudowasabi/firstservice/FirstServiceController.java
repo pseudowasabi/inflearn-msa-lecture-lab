cd163985580305e9b784593a1f04869668627bde
pseudowasabi <pseudowasabi@kakao.com>
2025-10-07 20:15:12 +0900

    (api-gateway-webflux) add routing config bean

--- first-service/src/main/java/com/pseudowasabi/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/pseudowasabi/firstservice/FirstServiceController.java
@@ -2,6 +2,7 @@ package com.pseudowasabi.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstServiceController {
 
     @GetMapping("/welcome")
-    public String welcome(){
+    public String welcome() {
         return "Welcome to first-service.";
+    }
+
+    @GetMapping("message")
+    public String message(@RequestHeader("f-request") String header) {
+        log.info("header={}", header);
+        return "Welcome to first-service with Request header: " + header;
     }
 }
